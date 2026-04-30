71802d71e1d0a53cd8b21e37c509f401b5c25013
lauchlinmac <lauchlinmac@gmail.com>
2026-04-30 12:44:21 -0400

    Create BatterPitcher.jsx
    V8

---
+++ src/components/BatterPitcher.jsx
@@ -0,0 +1,17 @@
+export default function BatterPitcher({ matchup }) {
+  if (!matchup) return null;
+
+  return (
+    <div className="matchup">
+      <div>
+        <h3>{matchup.batter.fullName}</h3>
+        <p>Batter</p>
+      </div>
+
+      <div>
+        <h3>{matchup.pitcher.fullName}</h3>
+        <p>Pitcher</p>
+      </div>
+    </div>
+  );
+}

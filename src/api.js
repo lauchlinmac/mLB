812dba8a58f7c46be8a4f80b873fbd590a974516
lauchlinmac <lauchlinmac@gmail.com>
2026-05-06 19:10:52 -0400

    Refactor fetch functions to use SCHEDULE_URL

--- src/api.js
+++ src/api.js
@@ -1,38 +1,36 @@
-// src/API.js
-
-const TODAY_URL =
-  "https://statsapi.mlb.com/api/v1/schedule?sportId=1&hydrate=team,linescore";
+const SCHEDULE_URL =
+  "https://statsapi.mlb.com/api/v1/schedule?sportId=1";
 
 export async function fetchGames() {
   try {
-    const response = await fetch(TODAY_URL);
+    const res = await fetch(SCHEDULE_URL);
 
-    if (!response.ok) {
-      throw new Error(`Schedule Error ${response.status}`);
+    if (!res.ok) {
+      throw new Error("Failed schedule fetch");
     }
 
-    const data = await response.json();
+    const data = await res.json();
 
     return data?.dates?.[0]?.games || [];
   } catch (err) {
-    console.error("FETCH GAMES ERROR:", err);
+    console.error(err);
     return [];
   }
 }
 
 export async function fetchLiveGame(gamePk) {
   try {
-    const response = await fetch(
+    const res = await fetch(
       `https://statsapi.mlb.com/api/v1.1/game/${gamePk}/feed/live`
     );
 
-    if (!response.ok) {
-      throw new Error(`Live Feed Error ${response.status}`);
+    if (!res.ok) {
+      throw new Error("Failed live fetch");
     }
 
-    return await response.json();
+    return await res.json();
   } catch (err) {
-    console.error("FETCH LIVE GAME ERROR:", err);
+    console.error(err);
     return null;
   }
 }
